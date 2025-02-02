2d57c0a53787dcaefec0394e6837807816d3524d
lucas.mathias <51675187+magicblak@users.noreply.github.com>
2025-02-02 13:15:15 -0300

    ajust section title color

--- utils/function.py
+++ utils/function.py
@@ -3,4 +3,4 @@ import streamlit as st
 def create_title(text):
     st.markdown(f'<h1 style="color: #fbba00;">{text}</h1>', unsafe_allow_html=True)
 def create_section_title(text):
-    st.markdown(f'<h1 style="color: #004580;">{text}</h1>', unsafe_allow_html=True)
+    st.markdown(f'<h1 style="color: #ed3237;">{text}</h1>', unsafe_allow_html=True)
