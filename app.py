28be8216f563358001606e783c2c6e13d192c2d8
lucas.mathias <51675187+magicblak@users.noreply.github.com>
2025-02-02 03:24:45 -0300

    init
    intial commit with the structure

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import json
+
+# Leitura de itens do menu
+file_path = 'menu.json'
+with open(file_path, 'r', encoding='utf-8') as file:
+    menu_pages = json.load(file)
+
+# Criação dos itens do menu
+menu = []
+pages_folder = 'views/'
+icons_folder = ':material/'
+for key, menu_page in menu_pages.items():
+    menu.append(
+        st.Page(
+            pages_folder + menu_page['file'],
+            title=menu_page['title'],
+            icon=icons_folder + menu_page['icon'],
+            default=bool(menu_page['default'])
+        )
+    )
+pg = st.navigation(
+    {
+        "Inicio": [menu[0]],
+        "Menu": menu[1:],
+    }
+)
+
+# Itens estáticos
+st.logo("assets/logo.png")
+st.sidebar.markdown("Acesse o código no [Github](https://github.com/magicblak/TechChallange-Passos-Magicos)")
+
+pg.run()
---
+++ views/home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Título principal
+st.title("Passos Mágicos")
+
+# Seção: Quem Somos
+st.header("Quem Somos")
+st.write("""
+A Associação Passos Mágicos tem uma trajetória de 31 anos de atuação, trabalhando na transformação da vida de crianças e jovens de baixa renda, proporcionando melhores oportunidades de vida.
+
+A transformação, idealizada por Michelle Flues e Dimetri Ivanoff, começou em 1992, atuando dentro de orfanatos no município de Embu-Guaçu.
+
+Em 2016, após anos de atuação, decidiram ampliar o programa para que mais jovens tivessem acesso a essa fórmula mágica de transformação que inclui: educação de qualidade, auxílio psicológico/psicopedagógico, ampliação da visão de mundo e protagonismo. Assim, passaram a atuar como um projeto social e educacional, criando a Associação Passos Mágicos.
+""")
