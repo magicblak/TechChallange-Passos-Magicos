5ebb7f97a17a35cceb2445c2552340645ad9d5c8
Lucas Mathias <51675187+magicblak@users.noreply.github.com>
2025-02-05 19:15:14 -0300

    Revert "Revert "Dashboard page""

---
+++ models/data_access.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+class DataAccess:
+    def __init__(self):
+        pass
+
+    def get_pm_db(self):
+        return pd.read_csv('data/base_pm_consolidada.csv')
--- utils/function.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def create_title(text):
-    st.markdown(f'<h1 style="color: #fbba00;">{text}</h1>', unsafe_allow_html=True)
-def create_section_title(text):
-    st.markdown(f'<h1 style="color: #004580;">{text}</h1>', unsafe_allow_html=True)
