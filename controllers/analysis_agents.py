bc044da4a9fc2a5df1611ab42777dd8934551ba1
lucas.mathias <51675187+magicblak@users.noreply.github.com>
2025-02-05 23:20:20 -0300

    asjust crewai agents and context

--- controllers/analysis_agents.py
+++ controllers/analysis_agents.py
@@ -9,8 +9,6 @@ from crewai_tools import SerperDevTool
 import tomli
 import json
 import os
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
 
 class StudentDashboardAgents():
     def __init__(self):
@@ -28,7 +26,6 @@ class StudentDashboardAgents():
         agent = Agent(
             role=role,
             goal=goal,
-            verbose=False,
             memory=False,
             backstory=(
                 backstory
@@ -56,65 +53,57 @@ class StudentDashboardAgents():
             process=Process.sequential,
             memory=True,
             cache=True,
-            max_rpm=100,
+            max_rpm=50,
+            max_execution_time=5000,
+            verbose=True,
             share_crew=True
         )
         return crew
     
-    async def __async_kickoff(self, crew):
-        loop = asyncio.get_event_loop()
-        with ThreadPoolExecutor() as pool:
-            result = await loop.run_in_executor(pool, crew.kickoff)
-        return result
-    
-    def request_analysis(self, indicators_explanation, cluster_explanation):
+    def request_analysis(self, base_explain, indicators_explanation, cluster_explanation):
         self.__get_credentials()
 
         indicator_explorer = self.__create_agent(
-            role="Explorador de Dados de Desempenho Acadêmico",
-            goal="Analisar os dados individuais do aluno e fornecer uma visão clara sobre onde ele está se destacando ou apresentando dificuldades em relação à média da turma.",
-            backstory="Especialista em avaliar o progresso acadêmico, adora identificar lacunas e oportunidades de crescimento por meio de dados objetivos. Focado em trazer insights precisos, mas com empatia."
+            role="Especialista de Dados de Desempenho Acadêmico",
+            goal="Observar e analisar os dados de desempenho acadêmico, buscando nuances que podem ser importantes na captura de comportamento do aluno",
+            backstory=f"""Especialista em avaliar a jornada acadêmica no ensino básico, adora identificar lacunas e oportunidades de crescimento por meio de dados objetivos. 
+                Focado em trazer insights precisos, mas com empatia.
+                Trabalha em uma ong que possui algumas informações importantes: {base_explain}"""
         )
         cluster_explorer = self.__create_agent(
             role="Especialista em Clusterização e Padrões",
-            goal="Fornecer insights sobre como o aluno se compara a outros grupos e como isso pode influenciar seu plano pedagógico.",
-            backstory="Especialista em analisar clusters de aprendizagem, identificar padrões relevantes e categorizar o aluno em grupos de similaridade.Apaixonado por descobrir padrões ocultos, este profissional traduz informações técnicas em dados simples e compreensíveis para educadores."
+            goal="Fornecer insights sobre como o comportamento do grupo onde o aluno se encontra e qual relação existe com os demais grupos, seja relação de igualdade ou divergência",
+            backstory=f"""Especialista em predição e análise de padrões/comportamento, adora identificar lacunas e oportunidades de crescimento por meio de dados objetivos. 
+                Focado em trazer insights precisos, mas com empatia.
+                Trabalha em uma ong que possui algumas informações importantes: {base_explain}"""
         )
         coach = self.__create_agent(
             role="Mentor Pedagógico Individual",
             goal="Propor ações específicas de apoio e desenvolvimento pedagógico, considerando as áreas de dificuldade e pontos fortes do aluno.",
-            backstory="Um orientador com experiência prática em sala de aula, preocupado com o desenvolvimento integral dos alunos. Ele traduz dados em estratégias concretas e humanas"
-        )
-        communicator = self.__create_agent(
-            role="Comunicador Educacional",
-            goal="Criar uma sintetização de informações que permita que diretores e coordenadores entendam rapidamente a situação acadêmica do aluno e saibam como agir.",
-            backstory="Com forte senso de empatia, este agente acredita que o segredo de um bom plano educacional é a comunicação clara entre dados e educadores."
+            backstory=f"""Um orientador com experiência prática em sala de aula, preocupado com o desenvolvimento integral dos alunos na ONG. Ele traduz dados em história
+                para que educadores possam avaliar e tomar ações
+                Trabalha em uma ong que possui algumas informações importantes: {base_explain}"""
         )
 
         indicator_explorer_task = self.__create_task(
             agent=indicator_explorer,
-            description="Avaliar as notas e indicadores de aprendizagem do aluno, comparando seu desempenho com a média da turma. Identificar pontos fortes e áreas de dificuldade.",
-            expected_output="Relatório em texto com as seguintes informações: Desempenho do aluno na fase (com destaque para áreas de alto e baixo desempenho). Comparação percentual com a média da turma. Pontos que precisam de atenção."
+            description="Avaliar as notas e indicadores de aprendizagem do aluno e/ou comparando seu desempenho com a média da turma. Identificar pontos fortes e áreas de dificuldade." + indicators_explanation,
+            expected_output="Relatório em texto apresentando evidências de lacunas e potenciais do estudante, incluindo análises longitudinais e comparativas, observando não só os indicadores, como também as notas de discplina do aluno (inglês, português e matemática)."
         )
         cluster_explorer_task = self.__create_task(
             agent=cluster_explorer,
-            description="Analisar o cluster ao qual o aluno pertence e identificar padrões de desempenho associados a esse grupo. Comparar suas características principais com outros alunos do mesmo cluster.",
-            expected_output="Relatório em texto de clusterização contendo: Cluster identificado e sua descrição (por exemplo, “Alunos com dificuldades no indicador X). Padrões comuns no grupo. Diferenças específicas do aluno em relação ao cluster."
+            description="Analisar o cluster ao qual o aluno pertence e identificar padrões de desempenho associados a esse grupo. Comparar suas características principais com outros alunos do mesmo cluster. E quais principais caracteristicas do melhor clustes em desempenho acadêmico." + cluster_explanation,
+            expected_output="Relatório em texto da análise de cluster, contendo padrões identificados e possibilidades de previsão futura, com a comparação de alunos que já passaram pela mesma fase."
         )
         coach_task = self.__create_task(
             agent=coach,
-            description="Construir um plano pedagógico individualizado com base nos dados de desempenho, evolução e clusterização. Sugerir intervenções específicas para reforçar as áreas de dificuldade e potencializar os pontos fortes.",
-            expected_output="Plano de intervenção  em texto contendo: Objetivos pedagógicos claros. Estratégias de apoio específicas (por exemplo, tutoria individual, práticas colaborativas). Recursos sugeridos (ex.: materiais adicionais ou sessões de reforço). Indicadores de monitoramento do progresso."
-        )
-        communicator_task = self.__create_task(
-            agent=communicator,
-            description="Compilar todas as informações geradas pelos outros agentes e sintetizá-las em uma ficha do estudante clara, objetiva e de fácil leitura para diretores e coordenadores.",
-            expected_output="Texto consolidado das informações do estudante, incluindo: Resumo geral do desempenho e evolução. Principais pontos de atenção e recomendações. Sugestão de acompanhamento pedagógico."
+            description="""Construir um storytelling do aluno por meio de informações do time de dados, e consolidar todas as análises em linguagem educacional seja de clusterização ou idicadores de desempenho acadêmico""" + base_explain,
+            expected_output="Texto em formato de storytelling para contar a história existente do aluno presente nos numeros, e plano de ação considerando a consolidação das análises em linguagem educacional, contendo: o futura análisado na clusterização e o presente e passado análisado nos inidicadores para potencializar e/ou auxiliar o launo"
         )
 
         crew = self.__create_crew(
-            agents=[indicator_explorer, cluster_explorer, coach, communicator],
-            tasks=[indicator_explorer_task, cluster_explorer_task, coach_task, communicator_task]
+            agents=[indicator_explorer, cluster_explorer, coach],
+            tasks=[indicator_explorer_task, cluster_explorer_task, coach_task]
         )
 
-        return asyncio.run(self.__async_kickoff(crew))
+        return crew.kickoff()
