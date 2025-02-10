cd75cab8fe73afe5f08e996b26a9b3ded8b69463
lucas.mathias <51675187+magicblak@users.noreply.github.com>
2025-02-10 18:28:18 -0300

    Update staff page

---
+++ views/about.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from utils.functions import (
+    create_title, 
+    create_about_member
+)
+
+create_title("Sobre a equipe")
+create_about_member('Carlos Santos', 'carlos', '', '')
+create_about_member('Fabia Bocayuva', 'fabia', '', '')
+create_about_member('Lucas Santos Mathias', 'lucas', 'http://www.linkedin.com/in/lucas-santos-mathias-990a54173', 'Atua com análise de dados educacionais há mais de 5 anos e acredita que para uma gestão educacional transformadora os dados são fundamentais.')
